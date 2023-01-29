fc5d4165c788c9acd53f2705a088b74c4ba3144f
Chris Hardgrove <63070357+tocohara@users.noreply.github.com>
2023-01-29 00:34:29 -0600

    Num sequence

---
+++ VS/BetaAI/BetaAI/DAC/AINumberingSequence.cs
@@ -0,0 +1,60 @@
+using System;
+using PX.Data;
+
+namespace BetaAI
+{
+  [Serializable]
+  [PXCacheName("AINumberingSequence")]
+  public class AINumberingSequence : IBqlTable
+  {
+    #region AISequenceID
+    [PXDBString(10, IsUnicode = true, InputMask = "")]
+    [PXUIField(DisplayName = "AI Sequence ID")]
+    public virtual string AISequenceID { get; set; }
+    public abstract class aISequenceID : PX.Data.BQL.BqlString.Field<aISequenceID> { }
+    #endregion
+
+    #region Tstamp
+    [PXDBTimestamp()]
+    [PXUIField(DisplayName = "Tstamp")]
+    public virtual byte[] Tstamp { get; set; }
+    public abstract class tstamp : PX.Data.BQL.BqlByteArray.Field<tstamp> { }
+    #endregion
+
+    #region CreatedByID
+    [PXDBCreatedByID()]
+    public virtual Guid? CreatedByID { get; set; }
+    public abstract class createdByID : PX.Data.BQL.BqlGuid.Field<createdByID> { }
+    #endregion
+
+    #region CreatedByScreenID
+    [PXDBCreatedByScreenID()]
+    public virtual string CreatedByScreenID { get; set; }
+    public abstract class createdByScreenID : PX.Data.BQL.BqlString.Field<createdByScreenID> { }
+    #endregion
+
+    #region CreatedDateTime
+    [PXDBCreatedDateTime()]
+    public virtual DateTime? CreatedDateTime { get; set; }
+    public abstract class createdDateTime : PX.Data.BQL.BqlDateTime.Field<createdDateTime> { }
+    #endregion
+
+    #region LastModifiedByID
+    [PXDBLastModifiedByID()]
+    public virtual Guid? LastModifiedByID { get; set; }
+    public abstract class lastModifiedByID : PX.Data.BQL.BqlGuid.Field<lastModifiedByID> { }
+    #endregion
+
+    #region LastModifiedByScreenID
+    [PXDBLastModifiedByScreenID()]
+    public virtual string LastModifiedByScreenID { get; set; }
+    public abstract class lastModifiedByScreenID : PX.Data.BQL.BqlString.Field<lastModifiedByScreenID> { }
+    #endregion
+
+    #region LastModifiedDateTime
+    [PXDBLastModifiedDateTime()]
+    public virtual DateTime? LastModifiedDateTime { get; set; }
+    public abstract class lastModifiedDateTime : PX.Data.BQL.BqlDateTime.Field<lastModifiedDateTime> { }
+    #endregion
+  }
+}
---
+++ VS/BetaAI/BetaAI/Graphs/AISetup.cs
@@ -0,0 +1,16 @@
+﻿using PX.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BetaAI
+{
+    public class AISetupMaint : PXGraph<AISetupMaint>
+    {
+        public PXSelect<AINumberingSequence> AISetupRecord;
+        public PXSave<AINumberingSequence> Save;
+        public PXCancel<AINumberingSequence> Cancel;
+    }
+}
